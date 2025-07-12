11c91cc84f891950bc86529ccd8bc4919035857d
Anjay Prasad <37243276+prasadanjay@users.noreply.github.com>
2025-07-13 01:27:28 +0530

    write documentation to my_square
    uses python "" operator

--- src/my_square.py
+++ src/my_square.py
@@ -1,6 +1,8 @@
 #Program to return the square of a number
 
 def my_square(x):
+    """Takes and value and returns the square of a number
+    """
    return(x ** 2)
 
 print("The square is  :", my_square(3))
