13c4825b28c5ef01ef048aafc18a42ab24c87945
Anjay Prasad <37243276+prasadanjay@users.noreply.github.com>
2025-07-13 10:08:02 +0530

    Added Documentation about ++ operator

--- src/my_square.py
+++ src/my_square.py
@@ -2,7 +2,8 @@
 
 def my_square(x):
     """Takes and value and returns the square of a number
+       Added more documentation about ++ operator
     """
-   return(x ** 2)
+    return(x ** 2)
 
 print("The square is  :", my_square(3))
