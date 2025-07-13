9cdec734608c064c29f4e3ebe64d1e959b323125
Anjay Prasad <prasad.anjay@gmail.com>
2025-07-13 11:11:52 +0530

    x -> y, an square of 42

--- src/my_square.py
+++ src/my_square.py
@@ -1,9 +1,9 @@
 #Program to return the square of a number
 
-def my_square(x):
+def my_square(y):
     """Takes and value and returns the square of a number
        Added more documentation about ++ operator
     """
-    return(x ** 2)
+    return(y ** 2)
 
-print("The square is  :", my_square(4))
+print("The square is  :", my_square(42))
