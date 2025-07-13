4ffba39b74c41f39e8f7a67ccb6c180f42ddd265
Anjay Prasad <prasad.anjay@gmail.com>
2025-07-13 18:34:47 +0530

    add and update documentation for squares

--- src/my_square.py
+++ src/my_square.py
@@ -3,10 +3,14 @@
 def my_square(y):
     """Takes and value and returns the square of a number
        Added more documentation about ++ operator
+       uses the ** operator
     """
     return(y ** 2)
 
 def my_square2(x):
+    """uses the * operator to calculate the  square
+
+    """
     return(x * x)
 
 print("The square is  :", my_square(42))
